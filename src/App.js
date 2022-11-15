40b2967a871909379115f5022deee8c16b6a593d
Antti Kurkinen <antti.kurkinen01@gmail.com>
2022-11-15 10:21:28 +0200

    korjattud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { chart} from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import GlobalTemperaturedata from "./data/GlobalTemperature.json";
 
 function App() {
+
+  const data = {
+    datasets: [
+      {
+        label: "Annually temperature",
+        data: [...GlobalTemperaturedata],
+        borderColor: "rgb(255, 99, 132",
+        backgroundColor: "rgba(255, 99, 132, 0.5",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "Annually(DEG C)", 
+        },
+        pointRadius: 1,
+      },
+      {
+        label: "Lowest temperature",
+        data: [...GlobalTemperaturedata],
+        borderColor: "#ff00ff",
+        backgroundColor: "rgba(255, 99, 132, 0.5",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "lowtemp", 
+        },
+        pointRadius: 1,
+      },
+      {
+        label: "Highest temperature",
+        data: [...GlobalTemperaturedata],
+        borderColor: "#00ffff",
+        backgroundColor: "rgba(255, 99, 132, 0.5",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "hightemp", 
+        },
+        pointRadius: 2,
+      }
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    Plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "demo Global temperature",
+      },
+    },
+    scales: {
+      Year: {
+        type: "linear",
+        display: true,
+        position: "right",
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "1000px" }}>
+        <Line options={options} data={data}/>
+      </div>
     </div>
   );
 }
