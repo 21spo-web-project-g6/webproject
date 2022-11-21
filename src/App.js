5eab2bffa7907fee028d33d353f6afae9255e7a5
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2022-11-21 14:19:09 +0200

    Hienosäätöä ennen Mergeä

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './Styles/App.css';
-import {Routes, Route } from "react-router-dom"
+import "./Styles/App.css";
+import { Routes, Route } from "react-router-dom";
 
-import V1 from './Routing/V1';
+import V1 from "./Routing/V1";
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-      
-        <Route path='/' element={<V1 />} />
+        <Route path="/V1" element={<V1 />} />
       </Routes>
     </div>
   );
