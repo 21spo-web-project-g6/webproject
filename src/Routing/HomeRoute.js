6e7cac2c8c2c89a5b6eb6597b0210273c75465e9
Juhani Koski <juhkukoskii@gmail.com>
2022-11-23 22:45:12 +0200

    .

--- src/Routing/HomeRoute.js
+++ src/Routing/HomeRoute.js
@@ -5,62 +5,9 @@ import HomeCSS from '../Styles/Home.module.css'
 export default function HomeRoute() {
   return (
       <div className={HomeCSS.container}>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article>
-          <p> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Dicta qui a laudantium exercitationem repudiandae dolore 
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
+        <div className={HomeCSS.content}>
+          <img src="../Images/bg.png" width="200px"/>
+        </div>
       </div>
   )
 }
