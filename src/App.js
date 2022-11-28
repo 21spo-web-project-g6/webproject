8c3aa3344e3ca60de51469b7bc2a61ba7221654c
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2022-11-28 19:58:26 +0200

    V5 Valmis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './Styles/App.css';
 import {Routes, Route } from "react-router-dom"
-
 import Home from './Routing/HomeRoute'
 import Login from './Routing/LoginRoute'
 import Register from './Routing/RegRoute'
 import V1 from "./Routing/V1";
+import V5 from "./Routing/V5";
 import Navbar from './Components/Navbar';
 
 export default function App() {
@@ -16,6 +16,7 @@ export default function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path="/V1" element={<V1 />} />
+        <Route path="/V5" element={<V5 />} />
       </Routes>
     </div>
   );
---
+++ src/Routing/V5.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import V5 from '../Components/V5'
+
+export default function RegRoute() {
+  return (
+    <>
+      <V5 />
+    </>
+  )
+}
