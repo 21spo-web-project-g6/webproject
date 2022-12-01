51753e8d9d7e897a1c8751d97639e8228dc9b94e
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2022-12-01 11:35:33 +0200

    V8 sivuvalikkoon

--- src/Routing/V8Route.js
+++ src/Routing/V8Route.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import Sidebar from '../Components/Sidebar'
 import V8 from '../Components/V8'
 
-export default function RegRoute() {
+import GraphCSS from '../Styles/Graph.module.css'
+
+export default function V8Route() {
   return (
-    <>
+    <div className={GraphCSS.wrapper}>
+      <Sidebar />
       <V8 />
-    </>
+    </div>
   )
 }
