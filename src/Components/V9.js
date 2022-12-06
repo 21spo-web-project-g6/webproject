0d699ad969a22a98b20e728756dcf982c4cc6e70
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2022-12-06 23:20:36 +0200

    Update V9.js

--- src/Components/V9.js
+++ src/Components/V9.js
@@ -1,5 +1,5 @@
 import { chart } from "chart.js/auto";
-import { Pie } from "react-chartjs-2";
+import { Line, Pie, Doughnut } from "react-chartjs-2";
 import { Chart } from "chart.js";
 import "chartjs-adapter-date-fns";
 import axios from "axios";
@@ -12,7 +12,9 @@ const URL14 = "http://localhost:3001/14";
 const URL15 = "http://localhost:3001/15";
 
 function V9() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks1, setTasks] = useState([]);
+  const [tasks2, setTasks2] = useState([]);
+  const [tasks3, setTasks3] = useState([]);
 
   useEffect(() => {
     axios
@@ -31,7 +33,7 @@ function V9() {
       .get(URL14)
       .then((response) => {
         console.log(response.data);
-        setTasks(response.data);
+        setTasks2(response.data);
       })
       .catch((error) => {
         alert(error.response.data.error);
@@ -43,22 +45,35 @@ function V9() {
       .get(URL15)
       .then((response) => {
         console.log(response.data);
-        setTasks(response.data);
+        setTasks3(response.data);
       })
       .catch((error) => {
         alert(error.response.data.error);
       });
   }, []);
 
-  const data = {
-    datasets: [
-      {
-        label: "Energy",
-        data: tasks,
-        borderColor: "rgba(255, 141, 96, 0.8)",
-        backgroundColor: "rgba(255, 141, 96, 0.8)",
+ const data = {
+  labels: [
+    'Energy',
+    'Industrial Processes',
+    'Waste',
+    "Agriculture, Forestry & Land Use (AFOLU)"
 
-      },
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [73.2, 5.2, 3.2, 18.4],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)',
+      'rgb(102, 255, 102)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+      
       // {
       //   label: "Industrial processes",
       //   data: tasks,
@@ -78,18 +93,37 @@ function V9() {
       //   borderColor: "rgba(15, 44, 233, 0.8)",
       //   backgroundColor: "rgba(15, 44, 233, 0.8)",
       // },
-    ],
-  };
+  
+  
 
   const config = {
     type: 'doughnut',
-    data: tasks,
+    radius: "50%",
+    data: data,
   };
 
   return (
     <div className={GraphCSS.container}>
-      <Pie options={config} data={data} />
+      <Doughnut options={config} data={data} />
       <p>
+    {tasks1.map((task1) => (
+    <p>
+      {task1.Sector} {task1.SectorPercentages} 
+    </p>
+    ))}
+
+{tasks2.map((task2) => (
+    <p>
+    {task2.SubSector} {task2.SubSectorPercentages}
+    </p>
+    ))}
+
+{tasks3.map((task3) => (
+    <p>
+    {task3.SubSector2} {task3.SubSectorPercentages2}
+    </p>
+    ))}
+
       </p>
       <h1><a href="http://carolynsnyder.com/papers/Snyder_Data_Figures.zip">Link</a> to data sources.</h1>
       <h1>Full <a href="https://climate.fas.harvard.edu/files/climate/files/snyder_2016.pdf">details.</a></h1>
